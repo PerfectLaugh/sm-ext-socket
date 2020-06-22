dec4ab11e148091efa0f0d81b7d6ae8d8603fdf5
PerfectLaugh <denniswu81229@gmail.com>
2020-06-22 13:36:47 +0800

    add travis-ci support

---
+++ build.bat
@@ -0,0 +1,25 @@
+set EXT_DIR="%cd%"
+set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
+
+if "%VSCMD_VER%"=="" (
+	set MAKE=
+	set CC=
+	set CXX=
+	call %VCVARSALL% x86
+)
+
+git clone https://github.com/alliedmodders/sourcemod --recursive --branch "%BRANCH%" --single-branch "%EXT_DIR%/sourcemod-%BRANCH%"
+
+; Boost
+wget https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.7z
+7z e boost_1_73_0.7z
+7z e boost_stage.7z -o boost_1_73_0
+
+mkdir "%EXT_DIR%/build"
+pushd "%EXT_DIR%/build"
+python "%EXT_DIR%/configure.py" --enable-optimize --sm-path "%EXT_DIR%/sourcemod-%BRANCH%" || goto error
+ambuild || goto error
+popd
+
+:error
+exit /b %errorlevel%
---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+EXT_DIR=$(pwd)
+
+git clone https://github.com/alliedmodders/sourcemod --branch $BRANCH --single-branch "$EXT_DIR/sourcemod-$BRANCH"
+
+# Boost
+wget https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.7z
+7z e boost_1_73_0.7z
+7z e boost_stage.7z -o boost_1_73_0
+
+mkdir -p "$EXT_DIR/build"
+pushd "$EXT_DIR/build"
+python3 "$EXT_DIR/configure.py" --enable-optimize --sm-path "$EXT_DIR/sourcemod-$BRANCH"
+ambuild
+
+# might be optional
+strip package/addons/sourcemod/extensions/socket.ext.so
+
+popd
